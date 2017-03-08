4e4896854f87b815b40708eda068cac3124f1fca
Tharindu Chathurange <tharindu@Tharindus-MacBook-Pro.local>
2017-03-08 20:17:23 +0530

    added tournaments to dashboard

---
+++ resources/views/tournaments/createTournament.blade.php
@@ -0,0 +1,21 @@
+@extends('layout_admin')
+
+
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+<!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Create Tournament
+        <small>Optional description</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+   
+</div>
+@endsection()
