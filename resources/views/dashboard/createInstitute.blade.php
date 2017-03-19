66ed1dcbbb68096a4be67761880fc453dec10a9b
Avishka Ruwanpathirana <avishkajapura@gmail.com>
2017-03-19 19:55:30 +0530

    validation display with danger style

--- resources/views/dashboard/createInstitute.blade.php
+++ resources/views/dashboard/createInstitute.blade.php
@@ -33,8 +33,14 @@
             
             {{csrf_field() }}
 
-            @if ($errors->any())
-                <ul>{!! implode('', $errors->all('<li style="color:red">:message</li>')) !!}</ul>
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
  
              <div class="box-body">
