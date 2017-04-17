1b9c3fef650853eae3b1645d085c10a9adc93c39
Tharindu Chathurange <tharindu@Tharindus-MacBook-Pro.local>
2017-04-18 00:51:51 +0530

    added match scheduling basic functionality

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Match;
 use Illuminate\Http\Request;
 
+use DB;
+
 class MatchController extends Controller
 {
     /**
@@ -17,7 +19,46 @@ class MatchController extends Controller
     }
     public function index()
     {
-        //
+        $tournaments = DB::table('tournaments')->get();
+        $pools = DB::table('pools')->get();
+        $teams = DB::table('teams')->get();
+        // $rounds = DB::table('rounds')
+        //             ->join('tournaments', 'rounds.tournament_id', '=', 'tournaments.id')
+        //             ->select('rounds.name')
+        //             ->get();
+        $matches = DB::table('matches')
+                    ->orderBy('id', 'desc')
+                    ->get();
+
+        // return view('tournaments.matchSchedule', compact('matches'));
+
+        return view('tournaments.matchSchedule',
+            [
+                'tournaments' => $tournaments,
+                'pools' => $pools,
+                'teams' => $teams,
+                'matches' => $matches
+            ]);
+    }
+
+    public function poolsDropDown($id)
+    {
+        // echo "<script>console.log( 'Debug Objects: ' );</script>";
+        // $round = DB::table('rounds')
+        //             ->where('tournament_id', $id)
+        //             ->firstOrFail();
+
+        // $roundID = $round->id;
+
+        // echo "<script>console.log( 'Debug Objects: " . $roundID . "' );</script>";
+
+        // $pools = DB::table("pools")
+        //             ->where("round_id", $roundID)
+        //             ->select("name","id")
+        //             // ->distinct()
+        //             ->get();
+                
+        // return json_encode($pools);
     }
 
     /**
@@ -38,7 +79,20 @@ class MatchController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $match = new Match;
+        $match->name = $request->matchName;
+        $match->format = $request->format;
+        $match->venue = $request->venue;
+        $match->matchStartDate = $request->startDate;
+        $match->matchEndDate = $request->endDate;
+        $match->team1_id = $request->team1;
+        $match->team2_id = $request->team2;
+        $match->tournament_id = $request->tournamentID;
+        $match->pool_id = $request->poolID;
+
+        $match->save();
+
+        return redirect('/matchSchedule');
     }
 
     /**
